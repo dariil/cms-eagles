3ec17c6ec117c6727395a39409fc7be10d0dda16
dariil <santiagojohndaryll@gmail.com>
2024-07-07 18:53:50 +0800

    Added case handler for when there is only 1 or no announcements

--- src/components/RMMEC/Announcements.jsx
+++ src/components/RMMEC/Announcements.jsx
@@ -8,8 +8,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function RMMECAnnouncements(){
-    const [latestAnnouncement, setLatestAnnouncement] = useState([]); /////   IMPORTANT    //////
-    const [secondLatestAnnouncement, setSecondLatestAnnouncement] = useState([]);
+    const [latestAnnouncement, setLatestAnnouncement] = useState(null); /////   IMPORTANT    //////
+    const [secondLatestAnnouncement, setSecondLatestAnnouncement] = useState(null);
     const [data, setData] = useState([]); /////   IMPORTANT    //////
 
     useEffect(() =>{
@@ -21,8 +21,13 @@ function RMMECAnnouncements(){
         try {
             await axios.get(`http://127.0.0.1:8000/api/getRecentAnnouncement/1`).then(function(response){
             console.log(response.data[0]);
-            setLatestAnnouncement(response.data[0]);
-            setSecondLatestAnnouncement(response.data[1]);
+            const announcements = response.data;
+            if (announcements.length > 0) {
+                setLatestAnnouncement(announcements[0]);
+            }
+            if (announcements.length > 1) {
+                setSecondLatestAnnouncement(announcements[1]);
+            }
             });
         } catch (error) {
             console.error('Error: ', error);
@@ -52,10 +57,7 @@ function RMMECAnnouncements(){
         return formattedDate;
     }
 
-    useEffect(() => {
-        console.log(latestAnnouncement.title);
-        // console.log(data[1].title);
-    }, [data]);
+
 
     return(
         <>
@@ -64,38 +66,43 @@ function RMMECAnnouncements(){
         <div className="announcement-main-container">
             <p className="latest-post-tag">Latest Announcements</p>
             <div className="latest-post-container">
-                <div className="latest-large">
-                    <a href="" className="announcement-prev-a">
-                        <div className="latest-l-img" style={{ backgroundImage: `url('http://localhost:8000/${latestAnnouncement.cover_image}')` }}></div>
-                        <div>
-                            <h3 className="latest-blog-l-title">{latestAnnouncement.title}</h3>
-                            <p>{latestAnnouncement.description}</p>
-                        </div>
-                        <div className="post-info-row">
-                            <div className="date-posted">
-                                {latestAnnouncement && latestAnnouncement.created_at && (
-                                <p><strong>Created at: </strong>{formatDate(latestAnnouncement.created_at.split('T')[0])}</p>
-                            )}
+                {latestAnnouncement && (
+                    <div className="latest-large">
+                        <a href="" className="announcement-prev-a">
+                            <div className="latest-l-img" style={{ backgroundImage: `url('http://localhost:8000/${latestAnnouncement.cover_image}')` }}></div>
+                            <div>
+                                <h3 className="latest-blog-l-title">{latestAnnouncement.title}</h3>
+                                <p>{latestAnnouncement.description}</p>
                             </div>
-                        </div>
-                    </a>
-                </div>
-                <div className="latest-small">
-                    <a href="" className="announcement-prev-a">
-                        <div className="latest-s-img" style={{ backgroundImage: `url('http://localhost:8000/${secondLatestAnnouncement.cover_image}')` }}></div>
-                        <div>
-                            <h3 className="latest-blog-l-title">{latestAnnouncement.title}</h3>
-                            <p>{secondLatestAnnouncement.description}</p>
-                        </div>
-                        <div className="post-info-row">
-                            <div className="date-posted">
-                                {secondLatestAnnouncement && secondLatestAnnouncement.created_at && (
-                                <p><strong>Created at: </strong>{formatDate(secondLatestAnnouncement.created_at.split('T')[0])}</p>
+                            <div className="post-info-row">
+                                <div className="date-posted">
+                                    {latestAnnouncement.created_at && (
+                                    <p><strong>Created at: </strong>{formatDate(latestAnnouncement.created_at.split('T')[0])}</p>
                                 )}
+                                </div>
                             </div>
-                        </div>
-                    </a>
-                </div>
+                        </a>
+                    </div>
+                )}
+
+                {secondLatestAnnouncement && (
+                    <div className="latest-small">
+                        <a href="" className="announcement-prev-a">
+                            <div className="latest-s-img" style={{ backgroundImage: `url('http://localhost:8000/${secondLatestAnnouncement.cover_image}')` }}></div>
+                            <div>
+                                <h3 className="latest-blog-l-title">{latestAnnouncement.title}</h3>
+                                <p>{secondLatestAnnouncement.description}</p>
+                            </div>
+                            <div className="post-info-row">
+                                <div className="date-posted">
+                                    {secondLatestAnnouncement.created_at && (
+                                    <p><strong>Created at: </strong>{formatDate(secondLatestAnnouncement.created_at.split('T')[0])}</p>
+                                    )}
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                )}
             </div>
 
             <h2>Other Announcements</h2>
