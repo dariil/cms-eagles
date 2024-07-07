fc7ae5f264ec97aa34b7bdb1862e69d6ed55322a
dariil <santiagojohndaryll@gmail.com>
2024-07-07 18:33:31 +0800

    Changed date format

--- src/components/RMMEC/Announcements.jsx
+++ src/components/RMMEC/Announcements.jsx
@@ -9,6 +9,7 @@ import "slick-carousel/slick/slick-theme.css";
 
 function RMMECAnnouncements(){
     const [latestAnnouncement, setLatestAnnouncement] = useState([]); /////   IMPORTANT    //////
+    const [secondLatestAnnouncement, setSecondLatestAnnouncement] = useState([]);
     const [data, setData] = useState([]); /////   IMPORTANT    //////
 
     useEffect(() =>{
@@ -21,6 +22,7 @@ function RMMECAnnouncements(){
             await axios.get(`http://127.0.0.1:8000/api/getRecentAnnouncement/1`).then(function(response){
             console.log(response.data[0]);
             setLatestAnnouncement(response.data[0]);
+            setSecondLatestAnnouncement(response.data[1]);
             });
         } catch (error) {
             console.error('Error: ', error);
@@ -38,91 +40,67 @@ function RMMECAnnouncements(){
         }
     }
 
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const options = {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric'
+        };
+        const formattedDate = date.toLocaleDateString(undefined, options);
+
+        return formattedDate;
+    }
+
     useEffect(() => {
         console.log(latestAnnouncement.title);
         // console.log(data[1].title);
     }, [data]);
 
-    // const PrevArrow = (props) => {
-    //     const { className, onClick } = props;
-    //     return (
-    //         <div className={className} onClick={onClick}>
-    //             <i className="fas fa-chevron-left"></i>
-    //         </div>
-    //     );
-    // };
-
-    // // Custom Next Arrow Component
-    // const NextArrow = (props) => {
-    //     const { className, onClick } = props;
-    //     return (
-    //         <div className={className} onClick={onClick}>
-    //             <i className="fas fa-chevron-right"></i>
-    //         </div>
-    //     );
-    // };
-
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        initialSlide: 0,
-        responsive: [
-          
-          
-          {
-            breakpoint: 1439,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ],
-      };
-
     return(
         <>
+        <div className="announcement-container">
         <Header></Header>
         <div className="announcement-main-container">
-            <p className="latest-post-tag">Latest Announcements (2)</p>
+            <p className="latest-post-tag">Latest Announcements</p>
             <div className="latest-post-container">
                 <div className="latest-large">
-                    <div className="latest-l-img"></div>
-                    <div>
-                        <h3 className="latest-blog-l-title">Lorem Ipsum dolor amet.</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                    </div>
-                    <div className="post-info-row">
-                        <div className="date-posted">
-                            <p><strong>Mar 19, 2022</strong> at 10:25 PM</p>
+                    <a href="" className="announcement-prev-a">
+                        <div className="latest-l-img" style={{ backgroundImage: `url('http://localhost:8000/${latestAnnouncement.cover_image}')` }}></div>
+                        <div>
+                            <h3 className="latest-blog-l-title">{latestAnnouncement.title}</h3>
+                            <p>{latestAnnouncement.description}</p>
                         </div>
-                    </div>
+                        <div className="post-info-row">
+                            <div className="date-posted">
+                                {latestAnnouncement && latestAnnouncement.created_at && (
+                                <p><strong>Created at: </strong>{formatDate(latestAnnouncement.created_at.split('T')[0])}</p>
+                            )}
+                            </div>
+                        </div>
+                    </a>
                 </div>
                 <div className="latest-small">
-                    <div className="latest-s-img"></div>
-                    <div>
-                        <h3 className="latest-blog-l-title">Lorem Ipsum dolor amet.</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                    </div>
-                    <div className="post-info-row">
-                        <div className="date-posted">
-                            <p><strong>Mar 19, 2022</strong> at 10:25 PM</p>
+                    <a href="" className="announcement-prev-a">
+                        <div className="latest-s-img" style={{ backgroundImage: `url('http://localhost:8000/${secondLatestAnnouncement.cover_image}')` }}></div>
+                        <div>
+                            <h3 className="latest-blog-l-title">{latestAnnouncement.title}</h3>
+                            <p>{secondLatestAnnouncement.description}</p>
                         </div>
-                    </div>
+                        <div className="post-info-row">
+                            <div className="date-posted">
+                                {secondLatestAnnouncement && secondLatestAnnouncement.created_at && (
+                                <p><strong>Created at: </strong>{formatDate(secondLatestAnnouncement.created_at.split('T')[0])}</p>
+                                )}
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
 
-        
+            <h2>Other Announcements</h2>
+            <hr></hr>
+
             <div className="main-parent-flex-container">
                 {
                     data.map((item, index) => (
@@ -132,13 +110,14 @@ function RMMECAnnouncements(){
                                 <h1 className="blogs-title">{item.title}</h1>
                                 <p className="blogs-intro">{item.description}</p>
                                 <div className="date-posted">
-                                    <p><strong>Created at: </strong>{item.created_at.split('T')[0]}</p>
+                                    <p><strong>Created at: </strong>{formatDate(item.created_at.split('T')[0])}</p>
                                 </div>
                             </a>
                         </div>
                     ))
                 }
             </div>
+        </div>
         </div>
         <Footer></Footer>
         </>
